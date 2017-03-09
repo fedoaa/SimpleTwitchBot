c1b0fe14803b62495a2f458fffcb7452709fc2b6
fedoaa <fedoaa@users.noreply.github.com>
2017-03-09 17:55:41 +0300

    Initial commit

---
+++ SimpleTwitchBot.Lib/IrcClient.cs
@@ -0,0 +1,62 @@
+﻿using System.IO;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace SimpleTwitchBot
+{
+    public class IrcClient
+    {
+        private readonly TcpClient _tcpClient;
+
+        private string _username;
+        private string _channel;
+        private StreamReader _inputStream;
+        private StreamWriter _outputStream;
+
+        public IrcClient()
+        {
+            _tcpClient = new TcpClient();
+        }
+
+        public async Task ConnectAsync(string ip, int port, string username, string password)
+        {
+            _username = username;
+
+            await _tcpClient.ConnectAsync(ip, port);
+            _inputStream = new StreamReader(_tcpClient.GetStream());
+            _outputStream = new StreamWriter(_tcpClient.GetStream());
+
+            _outputStream.WriteLine("PASS " + password);
+            _outputStream.WriteLine("NICK " + username);
+            _outputStream.WriteLine($"USER {username} 8 * :{username}");
+            _outputStream.Flush();
+        }
+
+        public void JoinRoom(string channel)
+        {
+            _channel = channel;
+
+            _outputStream.WriteLine("JOIN #" + channel);
+            _outputStream.WriteLine("CAP REQ :twitch.tv/tags");
+            _outputStream.WriteLine("CAP REQ :twitch.tv/commands");
+            _outputStream.Flush();
+        }
+
+        public void SendIrcMessage(string message)
+        {
+            _outputStream.WriteLine(message);
+            _outputStream.Flush();
+        }
+
+        public void SendChatMessage(string message)
+        {
+            SendIrcMessage(":" + _username + "!" + _username + "@" + _username + ".tmi.twitch.tv PRIVMSG #" + _channel + " :" + message);
+        }
+
+        public string ReadMessage()
+        {
+            string message = _inputStream.ReadLine();
+            return message;
+        }
+    }
+}
---
+++ SimpleTwitchBot.Lib/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SimpleTwitchBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string channelName = "kappa";
+
+            var client = new IrcClient();
+            
+            var connectionTask = client.ConnectAsync("irc.twitch.tv", 6667, "username", "oauth:token");
+            Task.WaitAll(connectionTask);
+            client.JoinRoom(channelName);
+
+            while (true)
+            {
+                string response = client.ReadMessage();
+                if (response.StartsWith("PING"))
+                {
+                    client.SendIrcMessage("PONG :tmi.twitch.tv");
+                    continue;
+                }
+                Console.WriteLine(response);
+            }
+
+        }
+    }
+}
